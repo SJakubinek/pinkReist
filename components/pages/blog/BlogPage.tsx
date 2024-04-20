eabb3a5535bca94d39b06ad68e5a803209378c72
SJakubinek <s.jakubinek@abydos.de>
2024-04-20 20:20:37 +0200

    fix: centered blog content

--- components/pages/blog/BlogPage.tsx
+++ components/pages/blog/BlogPage.tsx
@@ -28,49 +28,53 @@ export function BlogPage({ data, encodeDataAttribute }: BlogPageProps) {
 
   const createdDate = new Date(createdAt)
   return (
-    <div>
-      <div className="flex flex-row">
+    <div className="flex flex-col items-center">
+      <div className="w-5/6 lg:w-3/5">
         <div>
-          {/* Image  */}
-          <ImageBox
-            data-sanity={encodeDataAttribute?.('coverImage')}
-            image={coverImage}
-            // @TODO add alt field in schema
-            alt={coverImageAlt}
-            width={100}
-            height={100}
-            classesWrapper="relative aspect-auto"
-          />
-        </div>
-        <div className="place-content-center ml-4">
-          {/* Header */}
-          <Header title={title} description={overview} />
-          <div className="font-rockkitt text-lg">
-            {createdDate.toLocaleDateString('de-DE')}{' '}
-            {createdDate.toLocaleTimeString('de-DE')}
-          </div>
-        </div>
-      </div>
-      <div>
-        {/* Tags */}
-        <div className="py-3 lg:py-4 font-rockkitt">
-          <div className="text-xs md:text-sm">Tags</div>
-          <div className="text-md flex flex-row flex-wrap md:text-lg">
-            {tags?.map((tag, key) => (
-              <div key={key} className="mr-1 break-words">
-                #{tag}
+          <div className="flex flex-row">
+            <div>
+              {/* Image  */}
+              <ImageBox
+                data-sanity={encodeDataAttribute?.('coverImage')}
+                image={coverImage}
+                // @TODO add alt field in schema
+                alt={coverImageAlt}
+                width={100}
+                height={100}
+                classesWrapper="relative aspect-auto"
+              />
+            </div>
+            <div className="place-content-center ml-4">
+              {/* Header */}
+              <Header title={title} description={overview} />
+              <div className="font-rockkitt text-lg">
+                {createdDate.toLocaleDateString('de-DE')}{' '}
+                {createdDate.toLocaleTimeString('de-DE')}
               </div>
-            ))}
+            </div>
           </div>
+          <div>
+            {/* Tags */}
+            <div className="py-3 lg:py-4 font-rockkitt">
+              <div className="text-xs md:text-sm">Tags</div>
+              <div className="text-md flex flex-row flex-wrap md:text-lg">
+                {tags?.map((tag, key) => (
+                  <div key={key} className="mr-1 break-words">
+                    #{tag}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* Description */}
+          {body && (
+            <CustomPortableText
+              paragraphClasses="font-rockkitt max-w-3xl text-xl text-gray-600"
+              value={body}
+            />
+          )}
         </div>
       </div>
-      {/* Description */}
-      {body && (
-        <CustomPortableText
-          paragraphClasses="font-rockkitt max-w-3xl text-xl text-gray-600"
-          value={body}
-        />
-      )}
     </div>
   )
 }
